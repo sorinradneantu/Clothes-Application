da3a40d9158cf74d431ccfa9db608607db4f5b58
Cristian <cristian10.sicoe@gmail.com>
2020-06-15 19:44:34 +0300

    CA-35 #comment Create an Delete button

--- src/main/java/services/StoreService.java
+++ src/main/java/services/StoreService.java
@@ -3,10 +3,7 @@ package services;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import exceptions.CouldNotWriteProductsException;
-import exceptions.CouldNotWriteUsersException;
-import exceptions.ProductAlreadyExistsException;
-import exceptions.UsernameAlreadyExistsException;
+import exceptions.*;
 import model.Product;
 import model.User;
 import org.apache.commons.io.FileUtils;
@@ -17,6 +14,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
@@ -66,12 +64,68 @@ public class StoreService {
             }
         }
     }
+    public static void deleteData(String prodname,String username,String password)throws IOException,ProductDoesNotExist,ProductAlreadyExistsException{
+        Path pth;
+        int i = 0;
+        for (User user : users) {
+            if (!Objects.equals(username, user.getUsername()))
+                i++;
+        }
+        if(i==users.size()){
+            JOptionPane.showMessageDialog(null, "Failed security test");
+        }else {
+            for(User user:users){
+                if (Objects.equals(username, user.getUsername())){
+                    if (Objects.equals(user.getPassword(), UserService.encodePassword(username, password))) {
+                        List<Product> products;
+                        pth = FileSystemService.getPathToFile("config", username + ".json");
+                        products = loadDataFromFile(pth);
+                        checkProductDoesNotExist(prodname,pth);
+                        for (Iterator<Product> iter = products.listIterator(); iter.hasNext(); ) {
+                            Product a = iter.next();
+                            if (Objects.equals(a.getName(),prodname)) {
+                                iter.remove();
+                            }
+                        }
+                        try {
+                            FileWriter fwrite = new FileWriter(String.valueOf(pth));
+                            fwrite.write("[]");
+                            fwrite.close();
+                        } catch (IOException e) {
+                            System.out.println("An error occurred.");
+                            e.printStackTrace();
+                        }
+                        for(Product prod:products) {
+                            String tmp=String.valueOf(prod.getPrice());
+                            addData(prod.getName(), tmp, username, password);
+                        }
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "Failed security test");
+                    }
+                }
+            }
+        }
+    }
+
     private static void checkProductDoesNotAlreadyExist(String name,Path pth) throws ProductAlreadyExistsException,IOException {
         List<Product> products;
         products=loadDataFromFile(pth);
         for (Product prod:products) {
             if (Objects.equals(name, prod.getName()))
                 throw new ProductAlreadyExistsException(name);
+        }
+    }
+    private static void checkProductDoesNotExist(String name, Path pth) throws ProductDoesNotExist,IOException{
+        List<Product> products;
+        products=loadDataFromFile(pth);
+        int i=0;
+        for(Product prod:products){
+            if(!Objects.equals(name, prod.getName()))
+                i=i+1;
+        }
+        if(i==products.size()){
+            throw new ProductDoesNotExist(name);
         }
     }
 
